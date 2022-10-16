7146668fa8918f6d3274885abb946d685cd4203b
unknown <kodywinq1@gmail.com>
2022-10-16 14:56:19 +0300

    add: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = [];
+  let addition = [];
+  if(!("separator" in options)){
+    options["separator"] = "+";
+  }
+  if(!("additionSeparator" in options)){
+    options["additionSeparator"] = "|";
+  }
+  if(!("repeatTimes" in options)){
+    options["repeatTimes"] = 1;
+  }
+  if(!("additionRepeatTimes" in options)){
+    options["additionRepeatTimes"] = 1;
+  }
+  if(options["addition"] === null){
+    options["addition"] = "null";
+  }
+  for (let index = 0; index < options["additionRepeatTimes"]; index++) {
+    addition.push(options["addition"]);
+  }
+  addition = addition.join(options["additionSeparator"]);
+  addition = str+addition;
+  for (let index = 0; index < options["repeatTimes"]; index++) {
+    result.push(addition);
+  }
+  console.debug(result.join(options["separator"]));
+  return result.join(options["separator"]);
 }
 
 module.exports = {
